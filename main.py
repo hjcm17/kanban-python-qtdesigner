9cff560c3cf18c1b361a5ad000727385d5f99b1d
Hugo Mota <hugojcmota@gmail.com>
2023-12-17 00:57:38 -0300

    add new function

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 # Importando os modúlos PySide6
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QDialogButtonBox
 from PySide6.QtUiTools import QUiLoader
+
 import sys
 
 # Funções
 
-def abrir_janela_nova_tarefa():
+def abrir_janela_nova_tarefa(self):
     janela_nova_tarefa.show()
+
+def add_nova_tarefa():
+    try:
+        nova_tarefa = janela_nova_tarefa.line_nova_tarefa.text()
+        kanban_app.lw_a_fazer.addItem(nova_tarefa)
+    except:
+        print('não funcionou')
+
+        
 
 
 
@@ -24,6 +34,13 @@ janela_nova_tarefa = loader.load("ui_nova_tarefa.ui")
 # Comandos
 
 kanban_app.btn_a_fazer.clicked.connect(abrir_janela_nova_tarefa)
+
+# Conexões de caixa de botão
+
+
+
+janela_nova_tarefa.btn_box_nova_tarefa.accepted.connect(add_nova_tarefa)
+
 
 # Setando título
 '''
