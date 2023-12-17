5531e26c000242c2695c7965c93b1cfff6210753
Hugo Mota <hugojcmota@gmail.com>
2023-12-16 23:22:26 -0300

    loader the kanban app and title windows

---
+++ main.py
@@ -0,0 +1,45 @@
+# Importando os modúlos PySide6
+from PySide6.QtWidgets import QApplication
+from PySide6.QtUiTools import QUiLoader
+import sys
+
+# Funções
+
+def a_fazer():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+# Instanciamento
+
+app = QApplication(sys.argv)
+loader = QUiLoader()
+
+# Carregando arquivos de interface Qt Designer .ui
+
+kanban_app = loader.load("ui_main.ui")
+add_nova_tarefa = loader.load("ui_add_nova_tarefa.ui")
+
+# Comandos
+
+# Setando título
+titulo_main_window = "Kanban Board System"
+kanban_app.setWindowTitle(titulo_main_window)
+titulo_add_nova_tarefa = "Nova Tarefa"
+add_nova_tarefa.setWindowTitle(titulo_add_nova_tarefa)
+
+
+
+# Chamando janelas para execução
+kanban_app.show()
+
+sys.exit(app.exec())
